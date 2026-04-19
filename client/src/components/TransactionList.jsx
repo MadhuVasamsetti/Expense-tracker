39a46ac3e0431b422ee49e02a20f4a81aa158b04
Madhukar Vasamsetti <2300030709@kluniversity.in>
2026-04-19 19:50:28 +0530

    Update TransactionList.jsx

--- client/src/components/TransactionList.jsx
+++ client/src/components/TransactionList.jsx
@@ -6,7 +6,7 @@ function TransactionList({ transactions, filter }) {
 
   return (
     <div className="card">
-      <h3>Transa</h3>
+      <h3>Transact</h3>
       <ul>
         {filtered.map((tx) => (
           <li
